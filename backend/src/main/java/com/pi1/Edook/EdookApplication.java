70b20d66bf61760777c15cc444a24fc8a73d2ba2
esdras1110 <esdrasfacul709@gmail.com>
2026-06-04 10:52:03 -0300

    chore: adiciona a base do backend

---
+++ backend/src/main/java/com/pi1/Edook/EdookApplication.java
@@ -0,0 +1,13 @@
+package com.pi1.Edook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pi1/Edook/EdookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi1.Edook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
